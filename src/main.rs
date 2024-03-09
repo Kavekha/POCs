54dd5b0e2c26cd56e6b55b6737bc0bc63d9183d8
Kavekha <edelamaranthid@netcourrier.com>
2024-03-09 20:52:10 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 fn main() {
-    let world = generate_world();
-    generate_mission(world);
+    let world = World::new();
+    generate_story(world);
 }
 
-pub fn generate_mission(mut world: World){
+
+pub fn generate_story(world: World){
     println!("--- Fake-generated Mission ---");
 
     println!("Fixer says:");
@@ -18,35 +19,56 @@ pub fn generate_mission(mut world: World){
     println!("");
     println!("===========================");
     println!("");
-    println!("--- Generating Mission ---");
-    let target = generate_character("Meathead".to_string());
-    world.characters.push(target);
-    println!("world characters : {:?}.", world.characters);
-}
 
-pub fn generate_character(
-    name: String
-) -> Character {
-    let character = Character{
-        name
-    };
-    return character
-}
-
-#[derive(Debug, Clone)]
-pub struct Character{
-    pub name: String
+    let mission = Mission::new();
+    mission.add_target(world.create_character());
 }
 
 
-pub fn generate_world() -> World {
-    let world = World {
-        characters: Vec::new()
-    };
-    return world
+
+pub struct Mission{
+    targets: Vec<Character>,
+    client: Option<Character>
+}
+impl Mission{
+    pub fn new() -> Mission {
+        let mission = Mission {
+            targets: Vec::new(),
+            client: None
+        };
+        return mission 
+    }
+    pub fn add_target(mut self, target: Character) {
+        self.targets.push(target);
+    }
+    pub fn briefing(self){
+        println!("This is a briefing from this mission");
+    }
 }
 
-#[derive(Debug, Clone)]
+ 
+#[derive(Clone, Debug,)]
 pub struct World{
+    last_char_id: i32,
     pub characters: Vec<Character>
 }
+impl World{
+    pub fn new() -> World {
+        let world = World {
+            last_char_id: 0,
+            characters: Vec::new()
+        };
+        return world
+    }
+    pub fn create_character(mut self) -> Character {
+        let character = Character {};
+        self.last_char_id += 1;
+        self.characters.push(character.clone());
+        return character
+    }
+}
+
+
+
+#[derive(Clone, Debug)]
+pub struct Character {}
