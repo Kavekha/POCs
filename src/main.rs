1f49923e314e734b6b509a0ad001db458cf5b304
Kavekha <edelamaranthid@netcourrier.com>
2024-03-10 19:05:46 +0100

    Name generation, faction, background and rank

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use rand::prelude::*;
+
 fn main() {
     let mut world = World::new();
     generate_story(&mut world);
@@ -7,14 +9,17 @@ fn main() {
 pub fn generate_story(world: &mut World){
     println!("--- Fake-generated Mission ---");
 
-    println!("Fixer says:");
-    println!("Our client wants to remain anonym so we will call him 'Mr Johnson' as usual.");
-    println!("I'll be sure to apply the anonymous fee on him.");
-    println!("The target is Meathead, an ancient member of the Hell Angels gang going solo on the drug trade.");
-    println!("From the infos given by our mr Johnson, Meathead's hideout is in Anoubarak Station, abandonned a long time ago.");
-    println!("He's there with some friends and maybe a few locals he recruited to start his business.");
+    println!("Fixer says:");    // OK
+    println!("Our client wants to remain anonym so we will call him 'Mr Johnson' as usual.");   // OK // TODO : pronouns
+    println!("I'll be sure to apply the anonymous fee on him.");    // OK // TODO : Pronouns, payment modifier
+    println!("The target is Meathead, an ancient member of the Hell Angels gang going solo on the drug trade.");    // OK 
+    println!("From the infos given by our mr Johnson, Meathead's hideout is in Anoubarak Station, abandonned a long time ago.");    // NOK : //TODO : Hint & details on it. // Location
+    println!("He's there with some friends and maybe a few locals he recruited to start his business.");    // NOK : //TODO : Hint on target army / antagonists.
     println!("Kill him and come back in on piece to get paid.");
     println!("Budget is 3000 nuyens, 1000 in advance, plus a 400 nuyens fee for not being polite enough");
+    // TODO : Random info.
+    // TODO : Re-use created chars.
+    // TODO : Some kind of tags.
 
     println!("");
     println!("===========================");
@@ -30,10 +35,6 @@ pub fn generate_story(world: &mut World){
     //target
     let target = world.create_character();
     mission.add_target(target);
-    let target_two = world.create_character();
-    mission.add_target(target_two);
-    let target_three = world.create_character();
-    mission.add_target(target_three);
     //Type
     mission.set_objective(MissionType::Assassination);
     //Payment
@@ -42,6 +43,26 @@ pub fn generate_story(world: &mut World){
     //Read debriefing.
     mission.briefing();
 
+}
+
+pub fn generate_random_character_name() -> String {
+    let mut rng = rand::thread_rng();
+    let rand = rng.gen_range(0..10);
+    let mut name = "".to_string();
+    name += match rand {
+        0 => "Meathead",
+        1 => "Dandy",
+        3 => "Nacho",
+        4 => "Witch Hand",
+        5 => "Omen",
+        6 => "Limb Johnny",
+        7 => "Absolute Zero",
+        8 => "Alice",
+        9 => "Ace",
+        10 => "Adrian",
+        _ => "Akari"
+    };
+    return name
 }
 
 pub fn generate_mission(
@@ -90,50 +111,76 @@ impl Mission{
         self.base_payment = payment;
     }
     pub fn briefing(self){
-        match self.fixer {
-            None => {
-                println!("You heard about someone looking for runners, but can't quite identified them.");
-                return 
-            },
-            _ => println!("Fixer says:")
+        if let Some(fixer) = self.fixer {
+            println!("Your fixer {} says:", fixer.name);
+        } else {
+            println!("You heard about someone looking for runners, but can't quite identified them.");
+            return 
+        };
+        if let Some(client) = self.client {
+            println!("Our client is {}.", client.name);
+        } else {
+            println!("Our client wants to remain anonym so we will call him 'Mr Johnson' as usual.");
+            println!("I'll be sure to apply the anonymous fee on him.");
         }
-        match self.client {
-            None => {
-                println!("Our client wants to remain anonym so we will call him 'Mr Johnson' as usual.");
-                println!("I'll be sure to apply the anonymous fee on him.");
-            },
-            _ => println!("Our client is SomeOne.")
-        } 
-        match self.target {
-            None => {
-                println!("Our client has been enable to identify their target, and will come back to us when they do."); // vec.last()
+        if let Some(target) = self.target {
+            println!("The target is {}.", target.name); 
+            println!("{} was a {} for the {}, but is now working for {} as a {}.", target.name, target.previous_background.rank, target.previous_background.faction, target.current_background.faction, target.current_background.rank);
+        } else {
+            println!("Our client has been enable to identify their target, and will come back to us when they do."); // vec.last()
                 return 
-            },
-            _ => {
-                println!("The target is Meathead."); //println!("The target is Meathead, an ancient member of the Hell Angels gang going solo on the drug trade.");
-            }            
-        }        
+        };
         println!("Kill him and come back in on piece to get paid.");
         println!("Payment is {} nuyens.", self.base_payment); //println!("Budget is 3000 nuyens, 1000 in advance, plus a 400 nuyens fee for not being polite enough");
     }
 }
 
- 
+
 #[derive(Clone, Debug,)]
 pub struct World{
     last_char_id: i32,
-    pub characters: Vec<Character>
+    pub characters: Vec<Character>,
+    character_used_names: Vec<String>,
+    faction_used_names: Vec<String>
 }
 impl World{
     pub fn new() -> World {
         let world = World {
             last_char_id: 0,
-            characters: Vec::new()
+            characters: Vec::new(),
+            character_used_names: Vec::new(),
+            faction_used_names: Vec::new()
         };
         return world
     }
     pub fn create_character(&mut self) -> Character {
-        let character = Character {};
+        //Name
+        let mut name = generate_random_character_name();
+        //work bof
+        while self.character_used_names.contains(&name) {
+            name = generate_random_character_name();
+        };
+        self.character_used_names.push(name.clone());
+
+        //previous faction
+        let mut previous_faction_name = generate_random_faction_name();
+        while self.faction_used_names.contains(&previous_faction_name) {
+            previous_faction_name = generate_random_faction_name();
+        };
+        self.faction_used_names.push(previous_faction_name.clone());
+
+        //current faction
+        let mut current_faction_name = generate_random_faction_name();
+        while self.faction_used_names.contains(&current_faction_name) {
+            current_faction_name = generate_random_faction_name();
+        };
+        self.faction_used_names.push(current_faction_name.clone());
+
+        let character = Character {
+            name: name,
+            previous_background: generate_random_background(previous_faction_name.clone()),
+            current_background: generate_random_background(current_faction_name.clone())
+        };
         self.last_char_id += 1;
         self.characters.push(character.clone());
         //self.characters.push(character.clone());
@@ -141,7 +188,57 @@ impl World{
     }
 }
 
+pub fn generate_random_background(faction_name: String) -> Background {
+    let background = Background{
+        faction: faction_name,
+        rank: generate_random_rank_name()
+    };
+    return background
+}
+
+pub fn generate_random_faction_name() -> String {
+    let mut rng = rand::thread_rng();
+    let rand = rng.gen_range(0..10);
+    let mut name = "".to_string();
+    name += match rand {
+        0 => "Desolation Angels",
+        1 => "405 Hellhounds",
+        3 => "Black Rains",
+        4 => "Crimson Crush",
+        5 => "Octogone",
+        6 => "Société du Lotus Blanc",
+        7 => "Garde du Serpent Vert",
+        8 => "Halloweeners",
+        9 => "Razor Heads",
+        10 => "Scatterbrains",
+        _ => "Troll Killers"
+    };
+    return name
+}
+
+pub fn generate_random_rank_name() -> String {
+    let mut rng = rand::thread_rng();
+    let rand = rng.gen_range(0..10);
+    let mut name = "".to_string();
+    name += match rand {
+        0 => "Leader",
+        1|2|3 => "lieutenant",
+        4|5|6 => "Enforcer",
+        _ => "meatbag"
+    };
+    return name
+}
 
 
 #[derive(Clone, Debug)]
-pub struct Character {}
+pub struct Background {
+    pub faction: String,
+    pub rank: String
+}
+
+#[derive(Clone, Debug)]
+pub struct Character {
+    pub name: String,
+    previous_background: Background,
+    current_background: Background
+}
